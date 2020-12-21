c0a69b8b52d08b527baeeb3b3f06e3842caa3ec2
Simon Siefke <simon.siefke@gmail.com>
2020-12-21 19:42:57 +0100

    update example

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 import { forkPtyAndExecvp } from 'fork-pty'
 import { ReadStream } from 'tty'
 
-const fd = forkPtyAndExecvp('bash', ['bash', '-i'])
+const fd = forkPtyAndExecvp('bash', ['bash', '-c', 'echo hi'])
 
 const readStream = new ReadStream(fd)
 
