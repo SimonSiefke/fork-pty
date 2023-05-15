672b94a3593b79b50f49fa3628ee17cfb2da0b40
Simon Siefke <simon.siefke@gmail.com>
2023-05-15 13:38:22 +0200

    fix: benchmark

--- forkPty.benchmark.js
+++ forkPty.benchmark.js
@@ -1,14 +1,16 @@
 import { forkPtyAndExecvp } from './forkPty.js'
+import { ReadStream } from 'node:tty'
 
 const test1 = async () => {
   const s = performance.now()
   for (let i = 0; i < 100; i++) {
-    await new Promise((r) => {
-      const { fd, ptySocket } = forkPtyAndExecvp('ls', ['-l'])
+    await new Promise((resolve) => {
+      const { fd } = forkPtyAndExecvp('ls', ['-l'])
       let j = 0
+      const ptySocket = new ReadStream(fd)
       ptySocket.on('data', (data) => {
         ptySocket.destroy()
-        r()
+        resolve(undefined)
       })
     })
   }
